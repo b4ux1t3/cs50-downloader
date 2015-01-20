d7b0486bc63d8f188f0319a536cd0b5411d01f82
Chris P <flippedbauxite@gmail.com>
2015-01-20 10:42:11 -0500

    Fixed directories

---
+++ appliance/cs50dl_240p.py
@@ -0,0 +1,14 @@
+from os import system
+
+# Videos
+
+for i in range(12):
+    system("curl -o \"cs50_week%s_monday\" \"http://cdn.cs50.net/2014/fall/lectures/%s/m/week%sm-240p.mp4\"" % (i, i, i))
+    system("curl -o \"cs50_week%s_wednesday\" \"http://cdn.cs50.net/2014/fall/lectures/%s/w/week%sw-240p.mp4\"" % (i, i, i))
+
+    
+# Problem Sets
+
+for i in range(9):
+    system("curl -o \"pset%s_normal.html\" \"http://cdn.cs50.net/2015/x/psets/%s/pset%s/pset%s.html\"" % (i, i, i, i))
+    system("curl -o \"pset%s_hacker.html\" \"http://cdn.cs50.net/2015/x/psets/%s/hacker%s/hacker%s.html\"" % (i, i, i, i))
